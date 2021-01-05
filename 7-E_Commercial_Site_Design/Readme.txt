41b93335a56d2b5cabbf6ab525b332831078ed45
Ahmet Suhan OKA <58958906+ahmetsuhan@users.noreply.github.com>
2021-01-05 17:23:30 +0300

    Add files via upload
    Header section completed using with JQUERY, CSS and HTML.

---
+++ 7-E_Commercial_Site_Design/Readme.txt
@@ -0,0 +1,7 @@
+This project is fully responsive and made with just using HTML5 and CSS.
+
+
+/* *//////////////////
+1)Settings folder includes style file and js file format: .css and .js.
+2-)Img folder includes some image will be used by me in my web site.
+3-)Task folder has index.html file. From now on, index.html file is my task file to create task my design.
---
+++ 7-E_Commercial_Site_Design/Settings/script.js
@@ -0,0 +1,99 @@
+//JS Document
+
+$(document).ready(function() {
+
+
+
+    /*For Small Devices menu open button process >>>>>>>>>>*/
+    $("#headerMenuOpenButtonArea").click(function() {
+
+        $("#headerMenuOpenButtonMenuLimitArea").slideToggle("slow");
+    });
+    /*For Small Devices menu open button process <<<<<<<<<<*/
+
+
+    /*For Main and Footer  area top height calculation >>>>>>>>>>*/
+    var documentWidth = $(window).outerWidth();
+
+    if (!($("#headerMessageArea").length)) {
+        if (documentWidth >= 1200) { /* XL Device */
+            var offSetValue = 161;
+        } else if (documentWidth >= 992 && documentWidth <= 1199) { /* L Device */
+            var offSetValue = 390;
+        } else if (documentWidth >= 768 && documentWidth <= 991) { /* M device */
+            var offSetValue = 320;
+        } else if (documentWidth >= 576 && documentWidth <= 767) { /* S device */
+            var offSetValue = 193;
+        } else { /* XS Device*/
+            var offSetValue = 150;
+        }
+    } else {
+        if (documentWidth >= 1200) { /* XL Device */
+            var offSetValue = 273;
+        } else if (documentWidth >= 992 && documentWidth <= 1199) { /* L Device */
+            var offSetValue = 503;
+        } else if (documentWidth >= 768 && documentWidth <= 991) { /* M device */
+            var offSetValue = 433;
+        } else if (documentWidth >= 576 && documentWidth <= 767) { /* S device */
+            var offSetValue = 283;
+        } else { /* XS Device*/
+            var offSetValue = 228;
+        }
+    }
+    $("main").css("top", offSetValue);
+    $("footer").css("top", offSetValue);
+
+    $(window).resize(function() {
+        var documentWidth = $(window).outerWidth();
+
+        if (!($("#headerMessageArea").length)) {
+            if (documentWidth >= 1200) { /* XL Device */
+                var offSetValue = 161;
+            } else if (documentWidth >= 992 && documentWidth <= 1199) { /* L Device */
+                var offSetValue = 390;
+            } else if (documentWidth >= 768 && documentWidth <= 991) { /* M device */
+                var offSetValue = 320;
+            } else if (documentWidth >= 576 && documentWidth <= 767) { /* S device */
+                var offSetValue = 193;
+            } else { /* XS Device*/
+                var offSetValue = 150;
+            }
+        } else {
+            if (documentWidth >= 1200) { /* XL Device */
+                var offSetValue = 273;
+            } else if (documentWidth >= 992 && documentWidth <= 1199) { /* L Device */
+                var offSetValue = 503;
+            } else if (documentWidth >= 768 && documentWidth <= 991) { /* M device */
+                var offSetValue = 433;
+            } else if (documentWidth >= 576 && documentWidth <= 767) { /* S device */
+                var offSetValue = 283;
+            } else { /* XS Device*/
+                var offSetValue = 228;
+            }
+        }
+        $("main").css("top", offSetValue);
+        $("footer").css("top", offSetValue);
+    });
+    /*For Main and Footer  area top height calculation <<<<<<<<<<*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
