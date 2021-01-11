7c8734d112ef71d8612162d7ea39b971f86a2824
Ahmet Suhan OKA <58958906+ahmetsuhan@users.noreply.github.com>
2021-01-12 02:24:07 +0300

    Update scripts.js

--- 9_Phone_6S/scripts.js
+++ 9_Phone_6S/scripts.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
 
-
+    const user = {
+        password: "123456",
+        firstName: "Ahmet Suhan",
+        lastName: "Oka",
+        phoneNumber: "55555555555"
+    };
 
 
     const lockScreen = document.querySelector("#lockScreen");
+    const lockScreenCenterDate = document.querySelector("#lockScreenCenterDate");
+    const lockScreenBottom = document.querySelector("#lockScreenBottom");
+    const passwordScreen = document.querySelector("#passwordScreen");
     const mainButton = document.querySelector("#mainButton");
+    const passwordScreenCancel = document.querySelector(".passwordScreenBottomRightText");
+    let = passwordScreenButtons = document.querySelectorAll(".btnPasswordNumberContainer");
+    const passwordScreenButtonsValue = {
+        zero: passwordScreenButtons[0],
+        one: passwordScreenButtons[1],
+        two: passwordScreenButtons[2],
+        three: passwordScreenButtons[3],
+        four: passwordScreenButtons[4],
+        five: passwordScreenButtons[5],
+        six: passwordScreenButtons[6],
+        seven: passwordScreenButtons[7],
+        eight: passwordScreenButtons[8],
+        nine: passwordScreenButtons[9],
+    }
+
     var mainButtonClicked = false;
     var animationSettings = {
         animationDuration: "2000"
@@ -66,7 +89,10 @@ $(document).ready(function() {
     }
 
     function OpenPasswordScreen() {
-        console.log("Buraya ekran açma sayfasını yap kontrol et");
+
+        lockScreenCenterDate.style.display = "none"; //sonra flexe çekilecek
+        lockScreenBottom.style.display = "none"; //sonra flexe çekilecek
+        passwordScreen.style.display = "flex"; // sonra display none 
     }
 
     function LockScreen(e) {
@@ -74,9 +100,21 @@ $(document).ready(function() {
         $("#lockScreenBottomText").css({
             opacity: "0"
         });
+        lockScreenCenterDate.style.display = "flex"; //sonra flexe çekilecek
+        lockScreenBottom.style.display = "flex"; //sonra flexe çekilecek
+        passwordScreen.style.display = "none"; // sonra display none 
     }
 
 
+
+    /*Burada kaldın password kontrolü yap */
+    let passwordScreenNumberClickCounter = 0;
+    const PasswordScreenNumberClick = () => {
+        passwordScreenNumberClickCounter++;
+        if (passwordScreenNumberClickCounter >= user.password.length) {
+            passwordScreenNumberClickCounter = 0;
+        }
+    }
 
 
 
