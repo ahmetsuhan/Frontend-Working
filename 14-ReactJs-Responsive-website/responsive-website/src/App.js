89347231155736f2f11a56a3c9b694d82c9f3ae7
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-10 02:16:41 +0300

    Navbar component added

--- 14-ReactJs-Responsive-website/responsive-website/src/App.js
+++ 14-ReactJs-Responsive-website/responsive-website/src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> asdsd</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+    <Switch>
+    <Route path="/" exact>
+      
+    </Route>
+
+    </Switch>
+      </Router>
+      </>
   );
 }
 
