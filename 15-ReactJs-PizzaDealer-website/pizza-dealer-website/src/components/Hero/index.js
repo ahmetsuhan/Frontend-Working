51d96abb84fabd65c6d25192952f36cb58537a2b
Ahmet Suhan Oka <ahmetsuhanoka0@gmail.com>
2021-02-10 15:47:51 +0300

    Hero section added

---
+++ 15-ReactJs-PizzaDealer-website/pizza-dealer-website/src/components/Hero/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../Navbar';
+import {HeroContainer,HeroContent,HeroItems,HeroH1,HeroP,HeroBtn} from './HeroElements';
+
+function Hero() {
+    return (
+      <>
+        <HeroContainer>
+            <Navbar/>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Great Pizza Ever</HeroH1>
+                    <HeroP>Ready in 60 seconds</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
+        </HeroContainer>
+      </>
+    )
+}
+
+export default Hero
